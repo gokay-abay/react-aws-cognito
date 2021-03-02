6175a690c48a3c7b723f407e15f9de5b1f368959
Gökay Abay <abaygokay@gmail.com>
2021-03-02 11:32:26 -0800

    aws auth implemented

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,14 +1,38 @@
 import React, { useState } from "react"
+import { Auth } from "aws-amplify"
+import { useHistory } from "react-router-dom"
 
-export default function Register() {
+export default function Register({ authenticate }) {
   const [username, setUserName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const history = useHistory()
+
+  const submit = async (e) => {
+    e.preventDefault()
+    try {
+      //cognito register api
+      const signUpResponse = await Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email: email,
+        },
+      })
+      authenticate(true)
+      history.push("/protected")
+    } catch (error) {
+      // handle cognito error
+      console.log(error)
+    }
+  }
 
   return (
     <section className="section auth">
       <h1>Register</h1>
-      <form>
+      <form onSubmit={submit}>
         <p className="control">
           <input
             type="text"
@@ -34,6 +58,15 @@ export default function Register() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <input
+            className="input"
+            type="password"
+            id="confirmPassword"
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <input type="submit" value="submit" className="button is-success" />
         </p>
       </form>
     </section>
