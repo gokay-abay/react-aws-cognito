3d3d50d44eeecad83368ffdb0533395520f11e32
Gökay Abay <abaygokay@gmail.com>
2021-03-02 10:33:01 -0800

    aws

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import "bulma/css/bulma.min.css"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import Amplify from "aws-amplify"
+import config from "./config"
+
+Amplify.configure({
+  Auth: {
+    mandatorySignId: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
