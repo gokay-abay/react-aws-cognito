9e2314cd15e69a3b6b81eb0d98ea93d88e3cd8b0
Gökay Abay <abaygokay@gmail.com>
2021-03-02 09:47:37 -0800

    add Guarded component

---
+++ src/components/GuardedRoute.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+
+export default function GuardedRoute({ component: Component, auth, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        auth ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  )
+}
