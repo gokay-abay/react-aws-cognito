dbf39fbefb57d58cae0f6720c5db65d2ab1bc419
Gökay Abay <abaygokay@gmail.com>
2021-03-02 11:32:38 -0800

    login aws login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,11 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
+import { Auth } from "aws-amplify"
+import { useHistory } from "react-router-dom"
 
-const Login = () => {
+export default function Login({ authenticate }) {
+  const [username, setUserName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const history = useHistory()
+
+  const submit = async (e) => {
+    e.preventDefault()
+    try {
+      //cognito register api
+      const signInResponse = await Auth.signIn({
+        username,
+        password,
+      })
+      authenticate(true)
+      history.push("/protected")
+    } catch (error) {
+      // handle cognito error
+      console.log(error)
+    }
+  }
+
   return (
-    <div>
-      <p>Login</p>
-    </div>
+    <section className="section auth">
+      <h1>Login</h1>
+      <form onSubmit={submit}>
+        <p className="control">
+          <input
+            type="text"
+            className="input"
+            id="username"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+
+          <input
+            type="password"
+            className="input"
+            id="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <input type="submit" value="submit" className="button is-success" />
+        </p>
+      </form>
+    </section>
   )
 }
-
-export default Login
