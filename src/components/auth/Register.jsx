eda4238da2562e2a50a80f7208bfef9afbd7f33e
Gökay Abay <abaygokay@gmail.com>
2021-03-02 09:58:04 -0800

    add form to register page

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,11 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 
-const Register = () => {
+export default function Register() {
+  const [username, setUserName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
-    <div>
-      <p>Register</p>
-    </div>
+    <section className="section auth">
+      <h1>Register</h1>
+      <form>
+        <p className="control">
+          <input
+            type="text"
+            className="input"
+            id="username"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <input
+            type="email"
+            className="input"
+            id="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            className="input"
+            id="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </p>
+      </form>
+    </section>
   )
 }
-
-export default Register
